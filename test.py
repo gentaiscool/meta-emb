d3080f174a336f443a416740b5d54ecc4dfd232b
Genta Indra Winata <gentaindrawinata@gmail.com>
2019-10-06 22:40:06 +0800

    fix test.py

--- test.py
+++ test.py
@@ -11,7 +11,6 @@ import logging
 from tqdm import tqdm
 
 from models.transformer_tagger import TransformerTagger
-from models.lstm_tagger import LstmTagger
 from trainers.trainer import Trainer
 from utils import constant
 from utils.data import prepare_dataset
@@ -23,7 +22,7 @@ import numpy as np
 print("###### Initialize trainer  ######")
 trainer = Trainer()
 
-train_loader, valid_loader, test_loader, word2id, id2word, char2id, id2char, label2id, id2label, raw_test, bpe_embs = prepare_dataset(constant.params["train_file"], constant.params["valid_file"], constant.params["test_file"], constant.params["batch_size"], constant.params["batch_size"], train_valid=constant.params["train_valid"], stemming_arabic=constant.params["stemming_arabic"], new_preprocess=constant.params["new_preprocess"], bpe_lang_list=constant.params["bpe_lang_list"], bpe_vocab=constant.params["bpe_vocab"], bpe_emb_size=constant.params["bpe_emb_size"], bpe_cache=constant.params["bpe_cache"])
+train_loader, valid_loader, test_loader, word2id, id2word, char2id, id2char, label2id, id2label, raw_test, bpe_embs = prepare_dataset(constant.params["train_file"], constant.params["valid_file"], constant.params["test_file"], constant.params["batch_size"], constant.params["batch_size"], bpe_lang_list=constant.params["bpe_lang_list"], bpe_vocab=constant.params["bpe_vocab"], bpe_emb_size=constant.params["bpe_emb_size"], bpe_cache=constant.params["bpe_cache"])
 
 print("###### Prepare the dataset ######")
 
@@ -42,7 +41,7 @@ dropout = constant.params["dropout"]
 attn_dropout = constant.params["attn_dropout"]
 relu_dropout = constant.params["relu_dropout"]
 add_emb = constant.params["add_emb"]
-add_lstm = constant.params["add_lstm"]
+no_word_emb = constant.params["no_word_emb"]
 add_char_emb = constant.params["add_char_emb"]
 
 emb_list = constant.params["emb_list"]
@@ -53,31 +52,16 @@ bpe_lang_list = [] if constant.params["bpe_lang_list"] is None else constant.par
 bpe_emb_size = constant.params["bpe_emb_size"]
 bpe_vocab = constant.params["bpe_vocab"]
 
-lstm_num_layers = constant.params["lstm_num_layers"]
-lstm_dropout = constant.params["lstm_dropout"]
-lstm_bidirec = constant.params["lstm_bidirec"]
 mode = constant.params["mode"]
 no_projection = constant.params["no_projection"]
-proj_beta = constant.params["proj_beta"]
-orthogonalize = constant.params["orthogonalize"]
 
 cuda = constant.USE_CUDA
 pad_idx = 0
 use_crf = constant.params["use_crf"]
 
 print("######    Start training   ######")
-if constant.params["model"] == "TRFS":
-    # model = TransformerTagger(emb_size, hidden_size, num_layers, num_heads, dim_key, dim_value, filter_size, max_length, input_dropout, dropout, attn_dropout, relu_dropout, word2id, id2word, label2id, id2label, fasttext_en_emb_file=fasttext_en_emb_file, fasttext_es_emb_file=fasttext_es_emb_file, fasttext_pt_emb_file=fasttext_pt_emb_file, fasttext_it_emb_file=fasttext_it_emb_file, glove_en_emb_file=glove_en_emb_file, cuda=cuda, pad_idx=pad_idx, use_crf=use_crf, add_emb=add_emb, add_glove_emb=add_glove_emb, add_fasttext_pt_emb=add_fasttext_pt_emb, add_fasttext_it_emb=add_fasttext_it_emb)
-    model = TransformerTagger(emb_size, hidden_size, num_layers, num_heads, dim_key, dim_value, filter_size, max_length, input_dropout, dropout, attn_dropout, relu_dropout, word2id, id2word, char2id, id2char, label2id, id2label, char_emb_size=char_emb_size, char_hidden_size=char_hidden_size, emb_list=emb_list, cuda=cuda, pad_idx=pad_idx, use_crf=use_crf, add_emb=add_emb, add_lstm=add_lstm, add_char_emb=add_char_emb, lstm_num_layers=lstm_num_layers, lstm_dropout=lstm_dropout, lstm_bidirec=lstm_bidirec, mode=mode, no_projection=no_projection, orthogonalize=orthogonalize, proj_beta=proj_beta,
-    bpe_emb_size=bpe_emb_size, bpe_hidden_size=bpe_hidden_size, bpe_lang_list=bpe_lang_list, bpe_vocab=bpe_vocab, bpe_embs=bpe_embs)
-
-# elif constant.params["model"] == "LSTM":
-#     model = LstmTagger(cuda=constant.USE_CUDA, params=constant.params, pad_idx=word2id["<pad>"], word2id, id2word, label2id, id2label)
-else:
-    print("No model is selected")
-
-# print(model)
-# best_valid_f1, best_valid_loss, best_epoch = trainer.train(model, constant.params["task_name"], train_loader, valid_loader, test_loader, word2id, id2word, label2id, id2label, raw_test)
+model = TransformerTagger(emb_size, hidden_size, num_layers, num_heads, dim_key, dim_value, filter_size, max_length, input_dropout, dropout, attn_dropout, relu_dropout, word2id, id2word, char2id, id2char, label2id, id2label, char_emb_size=char_emb_size, char_hidden_size=char_hidden_size, emb_list=emb_list, cuda=cuda, pad_idx=pad_idx, use_crf=use_crf, add_emb=add_emb, add_char_emb=add_char_emb, no_word_emb=no_word_emb, mode=mode, no_projection=no_projection,
+bpe_emb_size=bpe_emb_size, bpe_hidden_size=bpe_hidden_size, bpe_lang_list=bpe_lang_list, bpe_vocab=bpe_vocab, bpe_embs=bpe_embs)
 
 if cuda:
     model = model.cuda()
@@ -93,7 +77,6 @@ with open("attention_scores.txt", "w+") as file_out:
     for i in range(len(attn_scores)):
         arr = attn_scores[i]
         for j in range(len(arr)):
-            # print(arr[j].shape)
             file_out.write(str(arr[j]))
         file_out.write("\n")
 
@@ -101,11 +84,8 @@ with open("bpe_attention_scores.txt", "w+") as file_out:
     for i in range(len(bpe_attn_scores)):
         arr = bpe_attn_scores[i]
         for j in range(len(arr)):
-            # print(arr[j].shape)
             file_out.write(str(arr[j]))
         file_out.write("\n")
-
-# print(attn_scores)
 
 all_predictions, all_true_labels, all_pairs, test_log_loss, attn_scores, bpe_attn_scores = trainer.evaluate(model, test_loader, word2id, id2word, label2id, id2label)
 metrics = measure(all_pairs)
